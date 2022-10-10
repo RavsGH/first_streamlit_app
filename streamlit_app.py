f5a674de6248e3dd8a3341da305f63283abf575c
RavsGH <91559363+RavsGH@users.noreply.github.com>
2022-10-10 16:36:31 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text(' (🥗 3 Kale, Spinach & Rocket Smoothie'
-streamlit.text('🐔 4 Hard-Boiled Free-Range Egg'
+streamlit.text(' (🥗 3 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 4 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
